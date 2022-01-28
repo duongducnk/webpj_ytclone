6bdc77f936d7dd489c4a7b9f4d30da35b8d94508
duongducnk <>
2022-01-28 15:23:35 +0000

    Upload pj

---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import {
+   getPopularVideos,
+   getVideosByCategory,
+} from '../../redux/actions/videos.action'
+import './_categoriesBar.scss'
+
+const keywords = [
+  'All',
+  'Live',
+  'Music',
+  'Sports',
+  'Cartoon',
+  'J-pop',
+  'Anime',
+  'Cooking',
+  'Game',
+  'Genshin Impact',
+  'Illustrations',
+  'Apex Legend',
+  'Monster Hunter',
+  'Recent Upload',
+  'Watched',
+  'New to you',
+]
+
+const CategoriesBar = () => {
+   const [activeElement, setActiveElement] = useState('All')
+
+   const dispatch = useDispatch()
+   const handleClick = value => {
+      setActiveElement(value)
+      if (value === 'All') {
+         dispatch(getPopularVideos())
+      } else {
+         dispatch(getVideosByCategory(value))
+      }
+   }
+
+   return (
+      <div className='categoriesBar'>
+         {keywords.map((value, i) => (
+            <span
+               onClick={() => handleClick(value)}
+               key={i}
+               className={activeElement === value ? 'active' : ''}>
+               {value}
+            </span>
+         ))}
+      </div>
+   )
+}
+
+export default CategoriesBar
